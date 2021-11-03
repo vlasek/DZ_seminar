83224bb7ece4dba939cdd4f1e3a8929a10fd6cd3
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-03 21:28:18 +0300

    два домашних на проверку

--- gia/Program.cs
+++ gia/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите  программу,  которая  в  последовательности  натуральных  чисел 
+// определяет минимальное число, оканчивающееся на 4. Программа получает 
+// на  вход  количество  чисел  в  последовательности,  а  затем  сами  числа.  
+// В последовательности всегда имеется число, оканчивающееся на 4. 
+// Количество  чисел  не  превышает 1000. Введённые  числа  не  превышают  
+// 30 000. 
+// Программа должна вывести одно число – минимальное число, оканчивающееся 
+// на 4.
+// var
+//   i, n, a, min: integer;
+
+// begin
+//   readln(n);
+//   min := 30000;
+//   for i := 1 to n do 
+//   begin
+//     readln(a);
+//     if (a mod 10 = 4) and (a < min) then
+//       min := a;
+//   end;
+//   writeln(min);
+// end
+
+int GetRandom ()
+{
+  return new Random().Next(100,30000);
+}
+
+int i=0, n, min=30000;
+Console.WriteLine ("Введите количество чисел массива (не более 1000):");
+n = Convert.ToInt32(Console.ReadLine ());
+
+int[] A = new int [n];
+while (i<n)
+{
+A[i]=GetRandom();
+i++;
+}
+i=0;
+while (i<n)
+{
+    if (((A[i]-4)%10==0) && (A[i]<min)) min=A[i];
+    i++;
+}
+Console.WriteLine("Массив чисел: ");
+i=0;
+while (i<n)
+{ Console.Write(A[i]+" "); i++;}
+
+Console.WriteLine();
+
+if (min!=30000) Console.WriteLine("Минимальное число, заканчивающиеся на 4 - это "+ min);
+else {Console.WriteLine("В данном массиве таких чисел не найдено");};
+
+
+
--- sobaka/Program.cs
+++ sobaka/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// два друга идут навстречу друг другу и между ними бегает задача. сколько раз она пробежеит между ними
+int count = 0;
+double distance = 10000;
+double firstFrSpeed = 1;
+double secondFrSpeed = 2;
+double dogSpeed = 5;
+int friend = 2;
+double time = 0;
+
+while (distance > 10)
+{
+
+    if (friend == 1)
+    {
+        time = distance / (firstFrSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFrSpeed + secondFrSpeed) * time;
+    }
+    else
+    {
+        time = distance / (secondFrSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFrSpeed + secondFrSpeed) * time;
+    }
+count++;
+}
+Console.WriteLine("Собака пробежит столько раз  "+count);
+
