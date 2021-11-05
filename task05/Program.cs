c5daf2792d12d84039413e4e0255160b58007b45
vlasek <92874711+vlasek@users.noreply.github.com>
2021-11-05 06:26:03 +0300

    5 зажача х2

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,3 +1,10 @@
 ﻿//Написать программу вычисления значения функции y=f(a)
 
-//пропускаем
+int Function(int x)
+{
+    int y = x * x ;
+    return y;
+}
+Console.WriteLine("Введите число перменную х: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ваш ответ"  + Function(a));
