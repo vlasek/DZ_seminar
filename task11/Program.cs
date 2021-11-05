901822564b5a03fb8e6550b7f3e2326de0f58a0e
vlasek <92874711+vlasek@users.noreply.github.com>
2021-11-05 06:20:42 +0300

    Update Program.cs
    добаивл само случ число

--- task11/Program.cs
+++ task11/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 int a=new Random().Next(10,99);
 
-Console.WriteLine (a);
+Console.WriteLine ("Случайное число " + a);
 Console.WriteLine ("Первая цифра " + a/10);
 
 Console.WriteLine ("Вторая цифра " + a%10);
