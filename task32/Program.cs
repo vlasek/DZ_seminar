fcec34ae1d9b1d40a5121795b5e7ba8ee038252e
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-06 21:28:50 +0300

    задание на 4

---
+++ task32/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+int GetRandom ()
+{
+  return new Random().Next(0,2);
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < 8; i++)
+    {Console.Write($"{(array[i])} ");}
+    Console.WriteLine();
+}
+int i=0;
+int[] A = new int [8];
+while (i<8)
+{ A[i]=GetRandom(); i++; }
+
+
+
+Console.WriteLine("Массив из 8 элементов");
+PrintArray (A);
---
+++ task36/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+
+int GetRandom ()
+{
+  return new Random().Next(100,1000);
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {Console.Write($"{(array[i])} ");}
+    Console.WriteLine();
+}
+
+int i=0;
+int[] A = new int [20];
+while (i<20)
+{ A[i]=GetRandom(); i++; }
+
+Console.WriteLine("Массив из 20 элементов");
+PrintArray (A);
+int nechet=0, chet=0;
+
+for (int j = 0; j < 20; j++)
+{
+    if (A[j]%2==0) chet=chet+1;
+    else nechet=nechet+1;
+}
+Console.WriteLine($"В массиве {nechet} нечетных элемпентов и {chet} четных элемента");
