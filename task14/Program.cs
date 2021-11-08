04ba5a25530df4835339bc1988409aa8ec370143
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-08 20:04:40 +0300

    f

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Найти третью цифру числа или сообщить, что её нет
-int a=new Random().Next(0,1000);
+int a=new Random().Next(1000);
 Console.WriteLine ("Случайное число "+a);
-if ((a>=100) && (a<1000)) Console.WriteLine ("Третья цифра "+a%10);
+if ((a>=100) && (a<1000)) Console.WriteLine ("Третья цифра "+a/100);
 else Console.WriteLine("Число не трехзначное  и третьей цифры нет");
 
 
