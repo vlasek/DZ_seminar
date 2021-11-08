69fd71d7537a34303f49b67be143d7442e124665
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-08 21:26:04 +0300

    d

---
+++ task22/Program.cs
@@ -0,0 +1,8 @@
+﻿//22 Найти расстояние между точками в пространстве 2D/3D
+//c2=a2+b2
+
+double Distanse(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+}
+Console.WriteLine(Distanse(0,0,2,2));
---
+++ task45/Program.cs
@@ -0,0 +1,14 @@
+﻿//45. Показать числа Фибоначчи
+
+int Fibonachi()
+{
+    int f=1, s=0;
+    for (int i =0; i<40; i++)
+    {
+    f=f+s;
+    s=f;
+    Console.WriteLine(f);
+    }
+    return f;
+}
+Console.WriteLine(Fibonachi());
