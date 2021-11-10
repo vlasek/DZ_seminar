0b295a0c8ad6407c64f8c54647eea77fe19b83a1
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-10 21:47:23 +0300

    middle

---
+++ task24/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти кубы чисел от 1 до N
+
+void metod (int n)
+{
+int i=1;
+while (i<n+1)
+{
+    Console.WriteLine($"{i} - {i*i*i} ");
+    i++;
+}
+}
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+metod(a);
