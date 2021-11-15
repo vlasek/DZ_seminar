61e2c7ea966382a189deb93e0c088a154d9b7af0
vlasek <92874711+vlasek@users.noreply.github.com>
2021-11-15 10:43:20 +0300

    Update Program.cs

--- task12/Program.cs
+++ task12/Program.cs
@@ -4,6 +4,8 @@ string Delete2Number (int n)
 {
 
     return $"{n/100}-{n%10}";
+    //return 10*(n)/100+n%10; проверить 
+    //Matn.Abs (number)' // модуль числа
 }
 
 int a=Convert.ToInt32(Console.ReadLine());
