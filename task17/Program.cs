c9287233da814802832461c23cf8df9837e99e44
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-18 19:42:20 +0300

    f

---
+++ task17/Program.cs
@@ -0,0 +1,13 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+
+int a, b;
+Console.WriteLine ("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine ());
+if (Math.Pow(b,2) == a || Math.Pow(a,2) == b) Console.WriteLine ("Да одно число квадрат второго");
+else {Console.WriteLine ("числа не являются квадратами друг друга");}
+
+
+//Console.WriteLine (Math.Pow(b,2)); // для проверки
---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+void metod (int n)
+{
+int i=1;
+while (i<n+1)
+{
+    Console.WriteLine($"{i} - {Math.Pow(i,2)} ");
+    i++;
+}
+}
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+metod(a);
