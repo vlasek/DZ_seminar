53b1784c2ccc37015412fb5e271e851e3809179a
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-18 19:05:30 +0300

    f

--- task07/Program.cs
+++ task07/Program.cs
@@ -1,33 +1,14 @@
 ﻿//Показать числа от -N до N
 
-int GetRandom ()
-{
-  return new Random().Next(1,3000);
-}
 
-int a, b, c, i=0 ;
-Console.WriteLine ("Введите начальный интервал");
+int a;
+Console.WriteLine ("Введите число N");
 a = Convert.ToInt32(Console.ReadLine ());
-Console.WriteLine ("Введите конченый интервал");
-b = Convert.ToInt32(Console.ReadLine ());
-//Console.WriteLine ("Введите количество элементов массива");
-//с = Convert.ToInt32(Console.ReadLine ());
 
-int[] A = new int [10];
-while (i<10)
+if (a<0) a=-a;
+for (int i=-a; i<=a; i++)
 {
-A[i]=GetRandom();
-i++;
+  Console.Write ($"{i},");
 }
-
-
-i=0;
-//while (i<n){
-while (i<10) 
-{
-    if ((A[i]>a) && (A[i]<b)) Console.Write(A[i]+" "); 
-    i++;}
-
-Console.WriteLine();
 
 
