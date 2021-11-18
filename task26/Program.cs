5d1083e24218f9a9faa3d2d0583e52ae996e9241
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-18 17:42:37 +0300

    26

---
+++ task26/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+int a, b, c=1;
+Console.WriteLine ("Введите число А");
+a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите степень Б");
+b=Convert.ToInt32(Console.ReadLine());
+
+
+for (int i=0; i<b; i++)
+{
+    c=c*a;
+}
+
+Console.WriteLine(c);
+
