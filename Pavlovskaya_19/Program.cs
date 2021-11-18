11a7ac99c4016ffef282a5ec9b63c60a9332c6b6
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-18 19:42:57 +0300

    f

---
+++ Pavlovskaya_19/Program.cs
@@ -0,0 +1,15 @@
+﻿double alpha=1;
+double z1=Math.Cos(alpha)+Math.Sin(alpha)+Math.Cos(3*alpha)+Math.Sin(3*alpha);
+Console.WriteLine (z1);
+
+double R=-7; x=1;
+
+double Y(double R, double X){
+
+
+if (R>=-9 && R<=-6)
+return Math.Sqrt(R*R- Math.Pow(x+6));
+if (R>-6 && R<=-3)
+return X;
+
+}
---
+++ Pavlovskaya_5_19/Program.cs
@@ -0,0 +1,59 @@
+﻿/* в одномерном массиве n
+вычислить произведение отрицательных элементов массива
+сумму положительных жлементов массива расположенных до максимального
+изменить порядок следования элементов в массиве на обратный
+*/
+
+int GetRandom ()
+{
+  return new Random().Next(-100,100);
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {Console.Write($"{(array[i])} ");}
+    Console.WriteLine();
+}
+int Proizv=1;
+
+int Proizvedenie(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {if (array[i]<0) Proizv=Proizv*array[i];}
+    Console.WriteLine();
+    return Proizv;
+}
+
+
+int i=0;
+int[] A = new int [20];
+int[] B = new int [20];
+while (i<20)
+{ A[i]=GetRandom(); i++; }
+PrintArray(A);
+
+// for (i = 0; i < A.Length; i++)
+//         {if (A[i]<0) Proizv=Proizv*A[i];}
+//     Console.WriteLine();
+Proizvedenie(A);
+Console.WriteLine ($"Произведение равно {Proizv}");
+
+int sum=0;
+int imax=0;
+for (i = 0; i < A.Length; i++)
+{if (A[i]>A[imax]) imax=i;}
+
+int Sum (int [] array, int imax)
+{for (i = 0; i < imax; i++)
+        {if (array[i]>0) sum=sum+array[i];}
+   
+    return sum;
+}
+
+
+Console.WriteLine ($"Сумма равна {Sum(A, imax)}");
+
+for (i = 0; i < A.Length; i++)    
+{B[i]=A[19-i];}
+PrintArray(B);
