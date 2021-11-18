edf71e45be10d391cc72a4cfb080b3c0ad003244
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-18 16:48:49 +0300

    41 задача методом

--- task02/Program.cs
+++ task02/Program.cs
@@ -5,4 +5,13 @@ a = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine ("Введите второе число");
 b = Convert.ToInt32(Console.ReadLine ());
 if (a<b) Console.WriteLine ("Первое меньше второго");
-else Console.WriteLine ("Второе больше первого");
+else {Console.WriteLine ("Второе больше первого");}
+
+// string res="";
+// string CheckDigit (int a, int b)
+// {
+//    if (a<b) {Console.WriteLine ("Первое меньше второго"); res="ybxtuj";}
+// else {Console.WriteLine ("Второе больше первого");} 
+// return res;
+// }
+// CheckDigit(a,b);
---
+++ task41/Program.cs
@@ -0,0 +1,23 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника 
+
+
+int a, b, c;
+Console.WriteLine ("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("Введите третье число");
+c = Convert.ToInt32(Console.ReadLine ());
+//int d=0;
+string CheckTriangle (int a, int b, int c)
+{
+    string result=String.Empty;
+    if (a+b>c && a+c>b && c+b>a) result=" Это стороны теругольника";
+        else {result=result + $" Это не стороны теругольника";}
+    return result;
+}
+
+Console.WriteLine(CheckTriangle(a, b, c));
+
+
+
